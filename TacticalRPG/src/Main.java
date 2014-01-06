680ed956c0ed00906fc244402131f8674bade70b
adelesse <alex.delesse@gmail.com>
2014-01-06 15:53:57 +0100

    Changement mineur, test pour vérifier que commit marche bien avec mon eclipse

--- TacticalRPG/src/Main.java
+++ TacticalRPG/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		UIExtended u = new UIExtended();
-		u.frameName("Advance Wars : Dark Conflict");
+		u.frameName("Zelda");
 		u.setFrameParametersToDefault();
 	}
 
