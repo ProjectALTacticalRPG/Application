b32dff6d6eefab9d06c45a218bd89966b4f2077f
Erset <jerome.p24@free.fr>
2014-01-21 01:20:59 +0100

    Tweak des controles pour empecher link de s'arreter quand on change de direction sans relacher la touche précédente

--- TacticalRPG/src/gameframework/expansion/MoveStrategyKeyboardExtended.java
+++ TacticalRPG/src/gameframework/expansion/MoveStrategyKeyboardExtended.java
@@ -6,12 +6,40 @@ import java.awt.event.KeyEvent;
 import gameframework.base.MoveStrategyKeyboard;
 
 public class MoveStrategyKeyboardExtended extends MoveStrategyKeyboard {
-	
+
+	private int lastKeyPressed;
+
+	@Override
+	public void keyPressed(KeyEvent event) {
+		int keycode = event.getKeyCode();
+		switch (keycode) {
+		case KeyEvent.VK_RIGHT:
+			speedVector.setDirection(new Point(1, 0));
+			lastKeyPressed = keycode;
+			break;
+		case KeyEvent.VK_LEFT:
+			speedVector.setDirection(new Point(-1, 0));
+			lastKeyPressed = keycode;
+			break;
+		case KeyEvent.VK_UP:
+			speedVector.setDirection(new Point(0, -1));
+			lastKeyPressed = keycode;
+			break;
+		case KeyEvent.VK_DOWN:
+			speedVector.setDirection(new Point(0, 1));
+			lastKeyPressed = keycode;
+			break;
+		}
+	}
+
 	@Override
 	public void keyReleased(KeyEvent event) {
 		int keycode = event.getKeyCode();
 		if(keycode == KeyEvent.VK_RIGHT || keycode == KeyEvent.VK_LEFT 
-				|| keycode == KeyEvent.VK_UP || keycode == KeyEvent.VK_DOWN)
-			speedVector.setDirection(new Point(0, 0));
+				|| keycode == KeyEvent.VK_UP || keycode == KeyEvent.VK_DOWN){
+			if(lastKeyPressed == keycode){
+				speedVector.setDirection(new Point(0, 0));
+			}
+		}
 	}
 }
