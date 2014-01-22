8df143df7290035e1d6c397a70a57a56c930ecdf
mat <mat@honoghr.emi.u-bordeaux1.fr>
2014-01-22 15:23:40 +0100

    Modification de la gestion d'aggro du keaton

--- TacticalRPG/src/gameframework/expansion/MoveStrategyKeaton.java
+++ TacticalRPG/src/gameframework/expansion/MoveStrategyKeaton.java
@@ -8,8 +8,13 @@ import java.awt.Point;
 import java.util.Random;
 
 public class MoveStrategyKeaton implements MoveStrategy {
-	SpeedVector move = new SpeedVectorDefaultImpl(new Point(0,
-			0), 4);
+	SpeedVector move = new SpeedVectorDefaultImpl(new Point(0,0), 4);
+	static Random random = new Random();
+	private int hold = 0;
+	private Point oldDirection;
+	private double distanceAggro = 250;
+	private double distanceDesaggro = 400;
+	private boolean inAggro = false;
 	Point goal, currentPosition;
 
 	public MoveStrategyKeaton(Point pos, Point goal) {
@@ -19,12 +24,48 @@ public class MoveStrategyKeaton implements MoveStrategy {
 
 	public SpeedVector getSpeedVector() {
 		double dist = currentPosition.distance(goal);
-		int xDirection = (int) Math.rint((goal.getX() - currentPosition.getX())
-				/ dist);
-		int yDirection = (int) Math.rint((goal.getY() - currentPosition.getY())
-				/ dist);
-		move.setDirection(new Point(xDirection,
-				yDirection));
+		double distAgg;
+		if(inAggro) {
+			distAgg = distanceDesaggro;
+		} else {
+			distAgg = distanceAggro;
+		}
+		if(dist > distAgg) {
+			inAggro = false;
+			if(hold <= 0){		
+				int i = random.nextInt(6);
+				switch (i) {
+				case 0:
+					move.setDirection(new Point(1, 0));
+					break;
+				case 1:
+					move.setDirection(new Point(-1, 0));
+					break;
+				case 2:
+					move.setDirection(new Point(0, -1));
+					break;
+				case 3:
+					move.setDirection(new Point(0, 1));
+					break;
+				case 4:
+					move.setDirection(new Point(0, 0));
+					break;
+				}				
+				if(i==4)
+					hold = 75;
+				else
+					hold = 4+random.nextInt(7);
+				oldDirection = move.getDirection();
+			} else 	{
+				--hold;
+				move.setDirection(oldDirection);
+			}
+		} else {
+			inAggro = true;
+			int xDirection = (int) Math.rint((goal.getX() - currentPosition.getX()) / dist);
+			int yDirection = (int) Math.rint((goal.getY() - currentPosition.getY()) / dist);
+			move.setDirection(new Point(xDirection, yDirection));
+		}
 		return move;
 	}
 	
