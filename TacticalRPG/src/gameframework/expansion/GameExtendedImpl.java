4ee4d5be67fc2da1236764c5be3b5ebd6a013cfe
Matthieu <matmatdu24@gmail.com>
2014-01-24 17:41:42 +0100

    Modification de la GUI

--- TacticalRPG/src/gameframework/expansion/GameExtendedImpl.java
+++ TacticalRPG/src/gameframework/expansion/GameExtendedImpl.java
@@ -1,13 +1,21 @@
 package gameframework.expansion;
 
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.Observable;
+
 import javax.swing.JFrame;
 
+import gameframework.game.CanvasDefaultImpl;
 import gameframework.game.GameDefaultImpl;
 
 public class GameExtendedImpl extends GameDefaultImpl {
 
 	private int resolutionW;
 	private int resolutionH;
+	
 	public GameExtendedImpl(int resolutionW, int resolutionH) {
 		super();
 		this.resolutionW = resolutionW;
@@ -18,7 +26,28 @@ public class GameExtendedImpl extends GameDefaultImpl {
 		f.setTitle(name);
 	}
 	
+	@Override
+	public void createGUI() {
+		f = new JFrame("Default Game");
+		f.dispose();
+		defaultCanvas = new CanvasDefaultImpl();
+		defaultCanvas.setSize(SPRITE_SIZE * NB_COLUMNS, SPRITE_SIZE * NB_ROWS);
+		f.add(defaultCanvas);
+		f.pack();
+		f.setVisible(true);
+
+		f.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
+	}
 	
+	@Override
+	public void update(Observable o, Object arg) {
+		
+	}
 	
 	public void setFrameParametersToDefault() {
 		defaultCanvas.setSize(resolutionW, resolutionH);
