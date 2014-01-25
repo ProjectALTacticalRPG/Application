05c709ad60cd66e2da1fcf01ac240e9cc2b8b3f2
Erset <jerome.p24@free.fr>
2014-01-25 03:17:30 +0100

    A NOTER DANS LE RAPPORT Fix d'un bug de collision dans le framework

--- TacticalRPG/src/gameframework/game/MoveBlockerCheckerDefaultImpl.java
+++ TacticalRPG/src/gameframework/game/MoveBlockerCheckerDefaultImpl.java
@@ -6,7 +6,6 @@ import gameframework.base.SpeedVector;
 
 import java.awt.Rectangle;
 import java.awt.Shape;
-import java.awt.geom.Area;
 import java.util.Vector;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
@@ -31,20 +30,24 @@ public class MoveBlockerCheckerDefaultImpl implements MoveBlockerChecker {
 		this.moveBlockerRuleApplier = moveBlockerRules;
 	}
 
+	/* Suppression des tests sur les Area car, en plus de ne pas en voir l'utilité, 
+	 * ces tests provoquent des bugs de collisions avec les déplacements en diagonale
+	 */
 	public boolean moveValidation(Movable m, SpeedVector mov) {
 		Shape intersectShape = IntersectTools.getIntersectShape(m, mov);
 		Vector<MoveBlocker> moveBlockersInIntersection = new Vector<MoveBlocker>();
-		Area intersectArea = new Area(intersectShape);
+		//Area intersectArea = new Area(intersectShape);
 		Rectangle tmpIntersec = (intersectShape.getBounds());
 
 		for (MoveBlocker moveBlocker : moveBlockers) {
 			Rectangle tmpB = moveBlocker.getBoundingBox();
 			if (tmpIntersec.intersects(tmpB)) {
+				moveBlockersInIntersection.add(moveBlocker);/*
 				Area tmpArea = new Area(tmpB);
 				tmpArea.intersect(intersectArea);
 				if (!tmpArea.isEmpty()) {
 					moveBlockersInIntersection.add(moveBlocker);
-				}
+				}*/
 			}
 		}
 
